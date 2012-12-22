d832ee34705ef1a307d8c18a2518c39c1308c34c
betachelsea <beta.chelsea@gmail.com>
2012-12-22 18:58:23 +0900

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FSlide::Application.config.secret_token = '24a0d40441509a34303331a51ef91d7645d83161953e755587be5636fbfd4b278e3312852a871554986e2efd66241d9038f1d0a68317f63f1377dfa527ff7fa0'
