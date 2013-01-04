05e918bfff11519ef3393fd33edd0cef88c2e046
betachelsea <beta.chelsea@gmail.com>
2013-01-05 01:01:18 +0900

    add button function

---
+++ app/assets/javascripts/system_setting.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	console.log("testests");
+	
+	//Next押下
+	/*
+	$(".buttons").bind("mouseover", function() {
+		var index = this.attr("id");
+		console.log("id->"+index);
+	});
+	*/
+
+	/*
+	$("#b-reset").bind("click", function() {
+		
+		//alert("reset clicked.");
+	});
+
+	$("#b-next").bind("click", function() {
+		alert("next clicked.");
+	});
+
+	$("#b-prev").bind("click", function() {
+		alert("prev clicked.");
+	});
+*/
+});
--- app/controllers/puzzlelist_controller.rb
+++ app/controllers/puzzlelist_controller.rb
@@ -6,7 +6,11 @@ class PuzzlelistController < ApplicationController
 		@msg = "hey!"
 		@filenames = PuzzleList.all
 		@prevfile = PuzzleList.find(0)
-		@showfile = PuzzleList.find(params[:id])
+		if params[:id] != nil
+			@showfile = PuzzleList.find(params[:id])
+		else
+			@showfile = PuzzleList.find(0)
+		end
 		@nextfile = PuzzleList.find(2)
 		@id = params[:id]
 	end
