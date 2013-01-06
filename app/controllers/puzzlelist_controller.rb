79031ca6fcf47e6f778d1ae37eb2a7446f57255c
betachelsea <beta.chelsea@gmail.com>
2013-01-06 11:38:57 +0900

    fix link paramerter

--- app/controllers/puzzlelist_controller.rb
+++ app/controllers/puzzlelist_controller.rb
@@ -5,14 +5,31 @@ class PuzzlelistController < ApplicationController
 	def f_puzzle
 		@msg = "hey!"
 		@filenames = PuzzleList.all
-		@prevfile = PuzzleList.find(0)
+		
+		#URLで指定されたパラメータを取得
+		@id = 1
 		if params[:id] != nil
-			@showfile = PuzzleList.find(params[:id])
-		else
-			@showfile = PuzzleList.find(0)
+			#数字以外の文字列のときはidはゼロとなる
+			@id = params[:id].to_i
 		end
-		@nextfile = PuzzleList.find(2)
-		@id = params[:id]
+		
+		#現在表示するデータを取得
+		@showfile = PuzzleList.find(@id)
+
+		#prev確認時に表示すべきデータのパラメータ取得
+		if @id == 0
+			@prevfile = PuzzleList.find(PuzzleList.count - 1)
+		else
+			@prevfile = PuzzleList.find(@id - 1)
+		end
+
+		#next確認時に表示すべきデータのパラメータ取得
+		if (@id + 1) == PuzzleList.count
+			@nextfile = PuzzleList.find(0)
+		else 
+			@nextfile = PuzzleList.find(@id + 1)
+		end
+
 	end
 
 end
