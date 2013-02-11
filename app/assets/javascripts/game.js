ba9db585ed2c248bd7c9a81e83047bc84bdabdfe
beta_chelsea <beta_chelsea@Johnny.local>
2013-02-11 23:59:52 +0900

    bugfix - didn't work piaces control when changing browser size

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -3,6 +3,10 @@ enchant();//use enchant.js
 var _click_x = 0;
 var _click_y = 0;
 
+//windowサイズ変更時のクリック座標不一致バグへの対策
+//読み込み時のenchant_stageの座標を保持
+var _init_stage_position;
+
 var _nowResourceNum = 0;
 var _playStartTime = -1;//-1:未測定、他：シャッフル開始時間
 var _record = 0;
@@ -11,10 +15,11 @@ var _root_partsimg = "./assets/";
 var _root_gifimg = "./assets/fukui/";
 var _PUZZLE_BOX_SIZE = 320;
 
-$("#test").text("aiuoe");
+$("#test").text("miow");
 
 function system_start(_filename, _filetext) {
 	//初期設定＆リソース読み込み他
+    _init_stage_position = $("#enchant-stage").offset();
 	var game = new Game(_PUZZLE_BOX_SIZE, _PUZZLE_BOX_SIZE);
 	game.fps = 15;
 	game.preload(_root_gifimg + _filename);
@@ -30,13 +35,15 @@ function system_start(_filename, _filetext) {
 				scene.addChild(slideBox.pieces[i]);
 			}
 		
-            scene.addEventListener('mousemove', function(e) {
-                $("#test").text("x->"+e.clientX+"y->"+e.clientY);
-            });
             //ゲーム開始・スライドパズルピース操作
 			scene.addEventListener('touchstart', function(e) {
-
-				var result = slideBox.operatePiece(e.clientX, e.clientY);
+               
+                //現在のenchant-stageのクリック位置を正確に取得
+                //アクセス後にブラウザサイズを変更されると誤差がでるため。
+				var bp = $("#enchant-stage").offset();
+                var result = slideBox.operatePiece(
+                                e.x + (_init_stage_position.left - bp.left),
+                                e.y + (_init_stage_position.top - bp.top));
                 if(result === 0) {//完成済みの状態のため、パズルをシャッフルさせる
                     var n = 0;
                     var shufflePuzzle = function() {
